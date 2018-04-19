4ac2b46aba6184e07cabe29c79f35955594e6d10
WangChao1227 <1174600867@qq.com>
2018-04-19 14:48:17 +0800

    formal PWT

--- Souces/main.c
+++ Souces/main.c
@@ -4,7 +4,7 @@
 #include "GPIO.h"
 #include "PWT.h"
 #include "UART.h"
-#include "stdio.h"//hello Git I am WangChao
+#include "stdio.h"
 
 #ifdef  __GNUC__
 #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
